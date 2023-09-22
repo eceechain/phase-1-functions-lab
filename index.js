b1a920c13ac4e4979513fd31fd141a4261ab18bf
ecee <you@example.com>
2023-09-22 13:29:23 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const headquarters = 42; 
+    return Math.abs(pickupLocation - headquarters); 
+}
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+function distanceFromHqInFeet(street) {
+    const blocks = distanceFromHqInBlocks(street);
+    const feetPerBlock = 264;
+    const distanceInFeet = blocks * feetPerBlock;
+    return distanceInFeet;
+  }
+  function distanceTravelledInFeet(startBlock, destinationBlock) {
+    const blocksTravelled = Math.abs(destinationBlock - startBlock);
+    const feetTravelled = blocksTravelled * 264;
+    return feetTravelled;
+  }
+  function calculatesFarePrice(startBlock, destinationBlock) {
+    const feetTravelled = Math.abs(destinationBlock - startBlock) * 264;
+    if (feetTravelled > 2500) {
+        return "not allowed";
+      }
+      if (feetTravelled <= 400) {
+        return 0;
+      }
+      else if (feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+      }
+      {
+        return 25;
+      }
+    }
